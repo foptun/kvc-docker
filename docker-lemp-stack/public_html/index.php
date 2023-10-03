a0200945b8333697fd4704d7be3a8973da9d2db1
foptun <oixuier.toppfun@gmail.com>
2023-10-03 18:47:01 +0700

    fix test ztvapi

--- docker-lemp-stack/public_html/index.php
+++ docker-lemp-stack/public_html/index.php
@@ -1,3 +1,12 @@
 <?php
 echo '<h1>Hello :: PHP Docker</h1>';
 ?>
+
+
+<script type='text/javascript' src='//www.doballmai.com/ztvapi/libary/x7ytw4ti2mmv.js'></script>
+
+<div id="mainPlayer"></div>
+
+<script type='text/javascript'>
+  createPlayer("nbt", "mainPlayer", "true", "50%", "live.png", "top-right");
+</script>
